eceb68a1e9ec15ce80059e3936a07784a0cc6584
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-14 15:58:40 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,24 @@
-#include "main.h"
+#include "main."
 
 /**
-* print_alphabet_x10 -Prints the alphabet 10 times.
-*
-* Return: Always 0 (Success)
-*/
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
 void print_alphabet_x10(void)
 {
-	int a, b;
+	char c;
+	int i = 0;
 
-	for (a = 0; a <= 0; a++)
+	while (i <= 9)
 	{
-	for (b = 97; b <= 122; b++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	_putchar(b);
+	_putchar(c);
 	}
+
 	_putchar('\n');
+
+	i++;
 	}
 }
