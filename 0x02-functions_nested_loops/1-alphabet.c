8c9bd27b1685b76e3959e5c81c6409272cf233d8
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-14 14:34:55 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-* print_alphabet - Entry point
+* print_alphabet - Prints the alphabet in lowercase.
 *
 * Return: Always 0 (Success)
 */
 
 void print_alphabet(void)
 {
-char i;
+	int i;
 
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
+	for (i = '97'; i <= '122'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
