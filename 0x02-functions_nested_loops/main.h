b9b66ff3f42d309c93c4da8fc11553f8f0f6ca30
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-14 06:19:45 -0700

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char);
 void print_alphabet(void);
-void print _alphabet_x10(void);
+void print '_alphabet_x10'(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
