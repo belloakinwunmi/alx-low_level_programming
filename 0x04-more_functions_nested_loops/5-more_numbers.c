2d561e94e2e5e500097d31010df10d02866ac207
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-15 15:49:10 -0700

    new5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,10 @@ void more_numbers(void)
 	{
 	for (j = 0; j < 15; j++)
 	{
-											if (j >= 10)
-											_putchar('1');
-											_putchar(j % 10 + '0');
-											}
+	if (j >= 10)
+	_putchar('1');
+	_putchar(j % 10 + '0');
+	}
 	_putchar('\n');
 	}
 }
