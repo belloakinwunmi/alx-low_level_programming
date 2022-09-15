4c6d5d596f2338ccf173c1b3b56870e05865c248
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-15 15:19:47 -0700

    07

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ *_isupper - check for uppercase character
+ *@c: The character to print
  *
- * Return: Always 0.
+ * Return: 1 if "c" is uppercase and 0 otherwise
  */
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
 	return (0);
 }
