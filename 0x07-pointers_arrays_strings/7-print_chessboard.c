fb6e46a08898102289521689b407794bcd400d52
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-26 07:35:14 -0700

    pas7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * print_chessboard - print a chessboard
- * @a:array
- * Return:void
+ * print_chessboard - Print the chessboard
+ * @a: array of pieces
+ *
+ * Return: Nothing.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int b, c;
 
-	for (i = 0; a[i][7]; i++)
+	for (b = 0; b < 8; b++)
 	{
-	for (j = 0; j < 8; j++)
-	_putchar(a[i][j]);
+	for (c = 0; c < 8; c++)
+	{
+	_putchar(a[b][c]);
+	}
 
 	_putchar('\n');
 	}
