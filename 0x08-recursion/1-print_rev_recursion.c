5e66d3ced02b227c2e2c3d6e25f40936aa5bb1f0
belloakinwunmi <belloakinwunmi@gmail.com>
2022-09-28 06:48:56 -0700

    r1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - print a string
- * @s:string
- * Return:void
+ * _print_rev_recursion - print revesed string .
+ * @s: string to print .
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
+	if (!*s)
+	return;
+
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-
-	}
 }
